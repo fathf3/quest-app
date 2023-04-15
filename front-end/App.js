de2ec26d5e6e7a8447fec75f3e6ba64770ad3a70
fathf3 <fatih-348@hotmail.com>
2023-04-15 17:36:45 +0300

    front-end folder update

---
+++ front-end/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Navbar from './components/Navbar/Navbar';
+import User from './components/User/User';
+import Home from './components/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/users/:userId" element={<User/>}></Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
